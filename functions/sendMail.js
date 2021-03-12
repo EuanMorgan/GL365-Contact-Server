3fa6c1ef6facdb21f057349cb9678ade5859d6f1
Euan Morgan <euanmorgan@Euans-MacBook-Pro.local>
2021-03-12 02:59:04 +0000

    add reply to

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -10,16 +10,17 @@ let transporter = nodemailer.createTransport({
 
 const send = async (name, email, subject, message, plainMessage) => {
   let mailOptions = {
-    from: "euanmorgan48@gmail.com",
-    to: "euanmorgan48@gmail.com",
+    from: email,
+    to: "carl@guitarlessons365.com",
     subject: subject,
-    text: `Submission of Contact Form:\nName: ${name}\nEmail:${email}\nSubject:${subject}\n\nMessage:\n${plainMessage}`,
+    text: `Submission of Contact Form:\nName: ${name}\nReply to:${email}\nSubject:${subject}\n\nMessage:\n${plainMessage}\n`,
     html: `<p style="font-weight:bold">
     <u>Submission of Contact Form:</u>
     </p><p style="font-weight:bold"> 
     <u>Name:</u> ${name}</p><br/>
-    <p style="font-weight:bold"> <u>Email:</u> <a href="mailto:${email}">${email}</a></p><br/>
-    <p style="font-weight:bold"> <u>Subject:</u> ${subject}</p><br/><p style="font-weight:bold"><u>Message:</u><br/> ${message}</p>`,
+    <p style="font-weight:bold"> <u>Reply to:</u> <a href="mailto:${email}">${email}</a></p><br/>
+    <p style="font-weight:bold"> <u>Subject:</u> ${subject}</p><br/><p style="font-weight:bold"><u>Message:</u><br/> ${message}</p><br/>`,
+    replyTo: email,
   };
   try {
     await transporter.sendMail(mailOptions);
